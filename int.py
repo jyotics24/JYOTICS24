2614d97c8332f9ee49e895cf3552e2daf89cd4bd
jyoti <jyotiprakashkhuntia999@gmail.com>
2024-02-11 19:43:36 +0530

    Add int file

---
+++ int.py
@@ -0,0 +1,13 @@
+import time 
+X1=17800 
+print("The value of X1 is:",X1)
+print()
+print("The data type is:",type(X1))
+print()
+X2=-12300 
+print("The value of X2 is:",X2)
+print()
+print("The data type is:",type(X2))
+print()
+time.sleep(2)
+print('End of an application')
